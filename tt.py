95baa5d333e18448f5b5e2e1af2b7992d2a60f5b
Louis Chevallier <louis-chevallier@users.noreply.github.com>
2022-01-06 19:08:20 +0100

    Update tt.py
    remove bad chars

--- tt.py
+++ tt.py
@@ -1,5 +1,5 @@
 
-
+from functools import reduce
 from lxml import etree
 with open("tt.html", "r") as fd :    html = fd.read()
 #print(html)
@@ -12,7 +12,8 @@ print(len(l))
 
 for ee in l :
     #print(ee[0].items())
-    title = ee[0].text.replace(' ', '_')
+    title = ee[0].text
+    title = reduce(lambda s, p : s.replace(p, '_'), " -()?/[]'`\",:", title) 
     print("youtube-dl https://www.youtube.com/" + dict(ee[0].items())['href'] + " -o %s -x --audio-format mp3 " )
 
 
