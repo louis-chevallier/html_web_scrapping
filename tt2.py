4795d00ccd1dfc27c17f7bf4120810d82cc2c039
Louis Chevallier <louis-chevallier@users.noreply.github.com>
2022-01-07 14:49:34 +0100

    Update tt2.py

--- tt2.py
+++ tt2.py
@@ -1,3 +1,23 @@
 
 
+from functools import reduce
+from lxml import etree
+with open("tt.html", "r") as fd :    html = fd.read()
+#print(html)
+dom = etree.HTML(html)
+e = dom.xpath("//aaa/div/ytd-grid-video-renderer/div/div")
+l = [ ee.xpath("div/h3/a") for ee in e]
+
+l = [ee for ee in l if len(ee) > 0]
+print(len(l))
+
+for ee in l :
+    #print(ee[0].items())
+    title = ee[0].text
+    title = reduce(lambda s, p : s.replace(p, '_'), " -()?/[]'`\",:", title) 
+    print("youtube-dl https://www.youtube.com/" + dict(ee[0].items())['href'] + " -o %s -x --audio-format mp3 " )
+
+
+
+
 
